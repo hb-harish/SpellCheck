328e2ee71619432f137c6fe9c4c69698d048648e
harish <hb.harishbalaji@gmail.com>
2016-09-15 11:36:39 -0400

    The spell checker

--- src/Checker.java
+++ src/Checker.java
@@ -4,23 +4,28 @@ public class Checker {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the sentence");
-		String w = sc.nextLine();
-		String delim = " ";
-		String[] parts = w.split(delim);
-		boolean flag = true;
+		
 		SpellCheck s = new SpellCheck();
-		for (int i =0; i<parts.length; i++)
+		while(true)
 		{
-			if (!s.Check(parts[i]))
+			System.out.println("Enter the sentence");
+			String w = sc.nextLine();
+			String delim = " ";
+			String[] parts = w.split(delim);
+			boolean flag = true;
+			for (int i =0; i<parts.length; i++)
 			{
-				System.out.println( parts[i] + " is not a word, please reenter");
-				flag = false;
+				if (!s.Check(parts[i]))
+				{
+					System.out.println( parts[i] + " is not a word, please reenter");
+					flag = false;
+				}
 			}
-		}
-		if (flag)
-		{
-			System.out.println("Good job");
+			if (flag)
+			{
+				System.out.println("Good job");
+				break;
+			}
 		}
 		
 	}
